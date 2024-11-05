f7e5ceac6af7160ce0027ffe6e9d103b8ec6ec10
jguimeram <jguimera54@gmail.com>
2024-11-05 06:46:47 +0100

    first iteration

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Board from './components/Board'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <Board />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Square.jsx
@@ -0,0 +1,12 @@
+
+function Square({ value }) {
+    
+    function handleClick() {
+        console.log('clicked')
+    }
+
+    return (
+        <button className="square" onClick={handleClick} >{value}</button>
+    )
+}
+  export default Square
